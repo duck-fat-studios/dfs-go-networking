1726b055bff8c188fb3f0a1bc98e536ee714cb5a
Eric Boxer <eric@duckfatstudios.com>
2023-10-25 08:50:44 -0400

    Made the NewUDPComms easer to use

--- udp/udp.go
+++ udp/udp.go
@@ -2,61 +2,65 @@ package dfsnetworking
 
 import "net"
 
-
 type UDPComms struct {
 	incomingConnection *net.UDPConn
 	outgoingConnection *net.UDPConn
-	readBuffer []byte
-	Handler func([]byte, *net.UDPAddr)
-	Interface *net.Interface
-	LocalIP net.IP
+	readBuffer         []byte
+	handler            UDPHandlerInterface
+	LocalIP            net.IP
 }
 
-
-func NewUDPComms(localIP net.IP, localInPort, localOutPort int, handler func([]byte, *net.UDPAddr), iface *net.Interface) (*UDPComms, error) {
-    inAddr := net.UDPAddr{
-        Port: localInPort,
-        IP:   localIP,
-    }
-    incomingConnection, err := net.ListenUDP("udp", &inAddr)
-    if err != nil {
-        return nil, err
-    }
-
-    outAddr := net.UDPAddr{
-        Port: localOutPort,
-        IP:   localIP,
-    }
-    outgoingConnection, err := net.ListenUDP("udp", &outAddr)
-    if err != nil {
-        return nil, err
-    }
-
-    return &UDPComms{
-        incomingConnection: incomingConnection,
-        outgoingConnection: outgoingConnection,
-        readBuffer:            make([]byte, 4096),
-        Handler:            handler,
-        Interface:          iface,
-        LocalIP:            localIP,
-    }, nil
+type UDPHandlerInterface interface {
+	Handle(data []byte, addr *net.UDPAddr)
 }
 
+func NewUDPComms(localIP net.IP,  handler UDPHandlerInterface, incomingPort int, outgoingPort ...int) (*UDPComms, error) {
+	inAddr := net.UDPAddr{
+		Port: incomingPort,
+		IP:   localIP,
+	}
+	incomingConnection, err := net.ListenUDP("udp", &inAddr)
+	if err != nil {
+		return nil, err
+	}
 
-func (u *UDPComms) Run() error{
+	var outgoingConnection *net.UDPConn
+	if len(outgoingPort) == 0 || outgoingPort[0] == incomingPort {
+		outgoingConnection = incomingConnection
+	} else {
+		outAddr := net.UDPAddr{
+			Port: outgoingPort[0],
+			IP:   localIP,
+		}
+		outgoingConnection, err = net.ListenUDP("udp", &outAddr)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &UDPComms{
+		incomingConnection: incomingConnection,
+		outgoingConnection: outgoingConnection,
+		readBuffer:         make([]byte, 4096),
+		handler:            handler,
+		LocalIP:            localIP,
+	}, nil
+}
+
+func (u *UDPComms) Run() error {
 	for {
 		buff := make([]byte, 4096)
 		n, addr, err := u.incomingConnection.ReadFromUDP(buff)
 
-		if err !=nil {
+		if err != nil {
 			return err
 		}
 
-		go u.Handler(buff[:n], addr)
+		go u.handler.Handle(buff[:n], addr)
 	}
 }
 
 func (u *UDPComms) Send(data []byte, remoteAddr *net.UDPAddr) error {
-    _, err := u.outgoingConnection.WriteToUDP(data, remoteAddr)
-    return err
+	_, err := u.outgoingConnection.WriteToUDP(data, remoteAddr)
+	return err
 }
